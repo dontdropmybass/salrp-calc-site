3f7006e0347a02e8f6c5f6501ed30f320a533643
Alexander Cochrane <dontdropmybass@gmail.com>
2018-06-18 02:49:47 -0300

    turns out "status 200" is an error... thanks jquery

--- script.js
+++ script.js
@@ -28,8 +28,29 @@ $(function() {
             }
             $("#charges").html(text);
         },
-        error: function(e) {
-            console.log(e);
+        error: function(charge) {
+            const table_header = "<tr><th>Offense</th><th>Fine</th><th>Jail Time</th></tr>";
+            let text = $("#charges").html();
+            console.log(categories.length);
+            console.log(charges.length);
+            for (i = 0; i < categories.length; i++) {
+                text += "<h1>" + categories[i] + "</h1><br/>";
+                text += "<table>" + table_header;
+                for (j = 0; j < charges.length; j++) {
+                    if (charges[j]["category"]===categories[i]) {
+                        charge = charges[j];
+                        text += "<tr id='charge"+j+"''><td>" + charge["offense"] + "</td>";
+                        text += "<td>" + charge["fine_amount"] + "</td>";
+                        text += "<td>" + charge["jail_time"] + "</td></tr>";
+                        $("#charge"+j).onhover(function() {
+                            $("#description").html(charge["description"]);
+                        });
+                        console.log(charge);
+                    }
+                }
+                text += "</table>";
+            }
+            $("#charges").html(text);
         }
     });
 });
