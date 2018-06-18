f2c634a0c420424e41c688cab753ecc57aee3fa3
Alexander Cochrane <dontdropmybass@gmail.com>
2018-06-18 03:05:25 -0300

    there was a semicolon

--- script.js
+++ script.js
@@ -1,56 +1,36 @@
+const table_header = "<tr><th>Offense</th><th>Fine</th><th>Jail Time</th></tr>";
+const categories = ["Traffic Violations", "Criminal Traffic Violations", "Criminal Offenses", "Public Acts"];
 $(function() {
-    console.log("start");
-    const categories = ["Traffic Violations", "Criminal Traffic Violations", "Criminal Offenses", "Public Acts"];
     $.ajax({
         dataType: "json",
         url: "offenses.json",
         success: function(charges) {
-            const table_header = "<tr><th>Offense</th><th>Fine</th><th>Jail Time</th></tr>";
-            let text = $("#charges").html();
-            console.log(categories.length);
-            console.log(charges.length);
-            for (i = 0; i < categories.length; i++) {
-                text += "<h1>" + categories[i] + "</h1><br/>";
-                text += "<table>" + table_header;
-                for (j = 0; j < charges.length; j++) {
-                    if (charges[j]["category"]===categories[i]) {
-                        charge = charges[j];
-                        text += "<tr id='charge"+j+"''><td>" + charge[0] + "</td>";
-                        text += "<td>" + charge[3] + "</td>";
-                        text += "<td>" + charge[4] + "</td></tr>";
-                        $("#charge"+j).onhover(function() {
-                            $("#description").html(charge[5]);
-                        });
-                        console.log(charge);
-                    }
-                }
-                text += "</table>";
-            }
-            $("#charges").html(text);
+            printCharges(charges);
         },
-        error: function(charges) {
-            const table_header = "<tr><th>Offense</th><th>Fine</th><th>Jail Time</th></tr>";
-            let text = $("#charges").html();
-            console.log(categories.length);
-            console.log(charges.length);
-            for (i = 0; i < categories.length; i++) {
-                text += "<h1>" + categories[i] + "</h1><br/>";
-                text += "<table>" + table_header;
-                for (j = 0; j < charges.length; j++) {
-                    if (charges[j]["category"]===categories[i]) {
-                        charge = charges[j];
-                        text += "<tr id='charge"+j+"''><td>" + charge[0] + "</td>";
-                        text += "<td>" + charge[3] + "</td>";
-                        text += "<td>" + charge[4] + "</td></tr>";
-                        $("#charge"+j).onhover(function() {
-                            $("#description").html(charge[5]);
-                        });
-                        console.log(charge);
-                    }
-                }
-                text += "</table>";
-            }
-            $("#charges").html(text);
+        error: function(e) {
+            printCharges(e);
         }
     });
 });
+
+function printCharges(charges) {
+    let text = $("#charges").html();
+    for (i = 0; i < categories.length; i++) {
+        text += "<h1>" + categories[i] + "</h1><br/>";
+        text += "<table>" + table_header;
+        for (j = 0; j < charges.length; j++) {
+            if (charges[j]["category"]===categories[i]) {
+                charge = charges[j];
+                text += "<tr id='charge"+j+"''><td>" + charge[0] + "</td>";
+                text += "<td>" + charge[3] + "</td>";
+                text += "<td>" + charge[4] + "</td></tr>";
+                $("#charge"+j).on('hover', function() {
+                    $("#description").html(charge[5]);
+                });
+                console.log(charge);
+            }
+        }
+        text += "</table>";
+    }
+    $("#charges").html(text);
+}
